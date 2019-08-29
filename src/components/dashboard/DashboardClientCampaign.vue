b7a719f7990a6b8ecf2ff70a029f65b0c5d59f19
Bohdan Vakhula <bvakhula@yandex.com>
2019-08-30 00:01:48 +0300

    startDate & endDate, updated compaings select to Autocomplete

--- src/components/dashboard/DashboardClientCampaign.vue
+++ src/components/dashboard/DashboardClientCampaign.vue
@@ -1,30 +1,99 @@
 <template>
   <div>
-    <v-card class="mb-3 py-5">
-      <v-layout justify-center wrap align-center row>
-        <v-spacer xs12 sm12 md4/>
-        <v-flex xs12 sm12 md4 row align-center justify-center>
-          <v-select
-              :items="clients"
-              label="Client"
-              v-model="selectedClientIds"
-              multiple
-              class="black--text"
-              item-text="name"
-              item-value="id"
-          ></v-select>
-          <v-select
-              v-model="selectedCampaignIds"
-              :items="campaigns"
-              label="Campaign"
-              multiple
-              class="black--text"
-              item-text="name"
-              item-value="id"
-              :disabled="!selectedClientIds.length"
-          ></v-select>
+    <v-card class="mb-3 pb-3">
+      <v-layout align-space-between justify-space-between row fill-height wrap>
+        <v-flex xs12 sm6 class="pa-2">
+          <v-card  min-height="400">
+            <v-card-title primary-title>
+              <v-flex>
+                <h2 class="mb-3">Clients</h2>
+                <v-select
+                    :items="clients"
+                    label="Client"
+                    v-model="selectedClientIds"
+                    multiple
+                    class="black--text"
+                    item-text="name"
+                    item-value="id"
+                ></v-select>
+                <v-layout row wrap justify-space-between align-center>
+                  <v-menu
+                    v-model="openStartDatePicker"
+                    :close-on-content-click="false"
+                    lazy
+                    transition="scale-transition"
+                    offset-y
+                    full-width
+                    min-width="290px"
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                        v-model="startDate"
+                        label="Compaigns start date"
+                        append-outer-icon="event"
+                        readonly
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker v-model="startDate" @input="openStartDatePicker = false" :max="endDate"></v-date-picker>
+                  </v-menu>
+                  <v-spacer class="mx-2 pickers-spacer"/>
+                  <v-menu
+                    v-model="openEndDatePicker"
+                    :close-on-content-click="false"
+                    lazy
+                    transition="scale-transition"
+                    offset-y
+                    full-width
+                    min-width="290px"
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                        v-model="endDate"
+                        label="Compaigns end date"
+                        append-outer-icon="event"
+                        readonly
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker v-model="endDate" @input="openEndDatePicker = false" :min="startDate"></v-date-picker>
+                  </v-menu>
+                </v-layout>
+                <v-btn block color="primary" :disabled="!selectedClientIds.length || !startDate || !endDate">Search Campaigns</v-btn>
+              </v-flex>
+            </v-card-title>
+          </v-card>
         </v-flex>
-        <v-spacer xs12 sm12 md4/>
+        <v-flex xs12 sm6 class="pa-2">
+          <v-card class="fill-height">
+            <v-card-title primary-title>
+              <v-flex>
+                <h2 class="mb-3">Campaigns</h2>
+                <v-autocomplete
+                  v-model="selectedCampaignIds"
+                  :disabled="!campaigns.length"
+                  :items="campaigns"
+                  chips
+                  label="Campaign"
+                  item-text="name"
+                  item-value="id"
+                  multiple
+                >
+                  <template v-slot:selection="data">
+                    <v-chip
+                      :selected="data.selected"
+                      close
+                      class="chip--select-multi"
+                      @input="removeCampaign(data.item)"
+                    >
+                      {{ data.item.name }}
+                    </v-chip>
+                  </template>
+                </v-autocomplete>
+              </v-flex>
+            </v-card-title>
+          </v-card>
+        </v-flex>
       </v-layout>
     </v-card>
     <v-layout row justify-end>
@@ -52,10 +121,26 @@ export default {
   },
   data () {
     return {
+      openStartDatePicker: false,
+      openEndDatePicker: false,
+      startDate: null,
+      endDate: null,
       selectedClientIds: [],
       selectedCampaignIds: []
     }
   },
-  methods: {}
+  methods: {
+    removeCampaign (item) {
+      const index = this.selectedCampaignIds.indexOf(item.id)
+      if (index >= 0) this.selectedCampaignIds.splice(index, 1)
+    }
+  }
 }
 </script>
+<style scoped lang="scss">
+.pickers-spacer {
+  @media screen and (max-width: 1125px) {
+    display: none;
+  }
+}
+</style>
